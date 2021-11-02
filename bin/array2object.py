9b1c9029fdd33fc7441c6619a4dcc2f8d7e741ca
Brett Adams <brett.whynot@gmail.com>
2021-11-03 09:31:01 +1000

    admin save

--- bin/array2object.py
+++ bin/array2object.py
@@ -66,7 +66,8 @@ class array2objectCommand(StreamingCommand):
             return self.loop_list(event,key,value)
         elif key not in event:
             # Add field for the first time
-            event = self.add_field(event,key,value)
+            self._record_writer.custom_fields.add(key)
+            event[key] = value
             #event[key] = value
         elif event[key] == None:
             # Replace null value
