bf4e04796aa2cdc597e03030a5dab30a2ec878e2
Brett Adams <brett.whynot@gmail.com>
2021-11-20 10:44:21 +1000

    admin save

--- bin/array2object.py
+++ bin/array2object.py
@@ -7,8 +7,6 @@ import json
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
 from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
 
-
-
 @Configuration()
 class array2objectCommand(StreamingCommand):
     field=Option(
