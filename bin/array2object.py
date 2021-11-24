2472a913b8a56221703539c5925a243e13956aee
Brett Adams <brett.whynot@gmail.com>
2021-11-24 11:34:47 +1000

    admin save

--- bin/array2object.py
+++ bin/array2object.py
@@ -4,7 +4,6 @@ import sys
 import os
 import json
 
-#sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
 
