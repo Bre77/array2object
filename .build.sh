badc53601f665ded94bf7b7843afaf7d4fd736d8
Brett <brett.whynot@gmail.com>
2024-09-12 13:38:01 +1000

    v1.0.10

--- .build.sh
+++ .build.sh
@@ -5,5 +5,7 @@ chmod -R u=rwX,go= *
 chmod -R u-x+X *
 chmod -R u=rwx,go= bin/*
 python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
+rm -rf lib/*/__pycache__
+rm -rf lib/*/*/__pycache__
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --overwrite array2object 
