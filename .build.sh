140591863756e082cca500e01115f5c2e119c9ae
Brett <brett.whynot@gmail.com>
2024-09-12 11:31:51 +1000

    v1.0.9

--- .build.sh
+++ .build.sh
@@ -3,11 +3,7 @@ cd "${0%/*}"
 OUTPUT="${1:-array2object.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
-chmod -R u=rwx,go= *
+chmod -R u=rwx,go= bin/*
 python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
-rm -rf lib/splunklib/__pycache__
-rm -rf lib/splunklib/searchcommands/__pycache__
-rm -rf lib/splunklib/modularinput/__pycache__
-rm -rf lib/*/__pycache__
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --overwrite array2object 
