dea15a4662aeb1d7917b1e3e40a8931160b7c74d
Brett <brett.whynot@gmail.com>
2024-11-19 11:06:24 +1000

    Cleanup lib

--- .build.sh
+++ .build.sh
@@ -4,8 +4,9 @@ OUTPUT="${1:-array2object.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
 chmod -R u=rwx,go= bin/*
+rm -rf lib/splunk*
 python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
 rm -rf lib/*/__pycache__
 rm -rf lib/*/*/__pycache__
 cd ..
-tar -cpzf $OUTPUT --exclude=.* --overwrite array2object 
+tar -cpzf $OUTPUT --exclude=.* --overwrite array2object
